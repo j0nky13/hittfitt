b13112f590d21a9ccbfbbc18084c94900982c4f7
Jonathan <jonkyle@me.com>
2025-09-04 01:13:17 -0400

    case sensitive fix

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/index.jsx'
-import Meals from './pages/meals/index.jsx'
-import Move from './pages/move/index.jsx'
-import About from './pages/about/index.jsx'
-import Success from './pages/success/index.jsx'
-import Cancel from './pages/cancel/index.jsx'
+import Home from './pages/Home.jsx'
+import Meals from './pages/Meals.jsx'
+import Move from './pages/Move.jsx'
+import About from './pages/About.jsx'
+import CheckoutSuccess from './pages/CheckoutSuccess.jsx'
+import CheckoutCancel from './pages/CheckoutCancel.jsx'
 
 export default function RoutesIndex() {
   return (
@@ -13,8 +13,8 @@ export default function RoutesIndex() {
       <Route path="/meals" element={<Meals />} />
       <Route path="/move" element={<Move />} />
       <Route path="/about" element={<About />} />
-      <Route path="/success" element={<Success />} />
-      <Route path="/cancel" element={<Cancel />} />
+      <Route path="/success" element={<CheckoutSuccess />} />
+      <Route path="/cancel" element={<CheckoutCancel />} />
       <Route path="*" element={<Home />} />
     </Routes>
   )
